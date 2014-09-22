7ca4dea15418b71da53dafd3b3a713d2cf129e83
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-09-22 22:05:39 +0200

    periph/gpio: use #if instead of #ifdef

--- drivers/include/periph/gpio.h
+++ drivers/include/periph/gpio.h
@@ -29,52 +29,52 @@
  * @brief Definition of available GPIO devices. Each device is managing exactly one pin.
  */
 typedef enum {
-#ifdef GPIO_0_EN
+#if GPIO_0_EN
     GPIO_0 = 0,             /**< GPIO device 0 */
 #endif
-#ifdef GPIO_1_EN
+#if GPIO_1_EN
     GPIO_1,                 /**< GPIO device 1 */
 #endif
-#ifdef GPIO_2_EN
+#if GPIO_2_EN
     GPIO_2,                 /**< GPIO device 2 */
 #endif
-#ifdef GPIO_3_EN
+#if GPIO_3_EN
     GPIO_3,                 /**< GPIO device 3 */
 #endif
-#ifdef GPIO_4_EN
+#if GPIO_4_EN
     GPIO_4,                 /**< GPIO device 4 */
 #endif
-#ifdef GPIO_5_EN
+#if GPIO_5_EN
     GPIO_5,                 /**< GPIO device 5 */
 #endif
-#ifdef GPIO_6_EN
+#if GPIO_6_EN
     GPIO_6,                 /**< GPIO device 6 */
 #endif
-#ifdef GPIO_7_EN
+#if GPIO_7_EN
     GPIO_7,                 /**< GPIO device 7 */
 #endif
-#ifdef GPIO_8_EN
+#if GPIO_8_EN
     GPIO_8,                 /**< GPIO device 8 */
 #endif
-#ifdef GPIO_9_EN
+#if GPIO_9_EN
     GPIO_9,                 /**< GPIO device 9 */
 #endif
-#ifdef GPIO_10_EN
+#if GPIO_10_EN
     GPIO_10,                /**< GPIO device 10 */
 #endif
-#ifdef GPIO_11_EN
+#if GPIO_11_EN
     GPIO_11,                /**< GPIO device 11 */
 #endif
-#ifdef GPIO_12_EN
+#if GPIO_12_EN
     GPIO_12,                /**< GPIO device 12 */
 #endif
-#ifdef GPIO_13_EN
+#if GPIO_13_EN
     GPIO_13,                /**< GPIO device 13 */
 #endif
-#ifdef GPIO_14_EN
+#if GPIO_14_EN
     GPIO_14,                /**< GPIO device 14 */
 #endif
-#ifdef GPIO_15_EN
+#if GPIO_15_EN
     GPIO_15,                /**< GPIO device 15 */
 #endif
 } gpio_t;
