f372e08b8e3de7ec8e0c7c85423415374b377dec
Rane Balslev <ranebalslev@gmail.com>
2014-10-21 14:10:24 +0200

    test

--- cpu/samd21/include/cpu-conf.h
+++ cpu/samd21/include/cpu-conf.h
@@ -27,13 +27,13 @@
  * TODO: measure and adjust for the cortex-m0
  * @{
  */
-#define KERNEL_CONF_STACKSIZE_PRINTF    (256)
+#define KERNEL_CONF_STACKSIZE_PRINTF    (512)
 
 #ifndef KERNEL_CONF_STACKSIZE_DEFAULT
-#define KERNEL_CONF_STACKSIZE_DEFAULT   (1024)
+#define KERNEL_CONF_STACKSIZE_DEFAULT   (512)
 #endif
 
-#define KERNEL_CONF_STACKSIZE_IDLE      (512)
+#define KERNEL_CONF_STACKSIZE_IDLE      (256)
 /** @} */
 
 /**
@@ -43,11 +43,11 @@
  * @{
  */
 #ifndef UART0_BUFSIZE
-#define UART0_BUFSIZE                   (256)
+#define UART0_BUFSIZE                   (128)
 #endif
 /** @} */
 
-#define TRANSCEIVER_BUFFER_SIZE (64)
+#define TRANSCEIVER_BUFFER_SIZE (10)
 
 
 #endif /* __CPU_CONF_H */
